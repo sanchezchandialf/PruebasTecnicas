6ef4901c77a363ab8b9dd266b466cd917b3076b3
sanchezchandialf <lautarosanche@gmail.com>
2025-05-28 06:56:27 -0300

    ejercicio #01

--- MaquinaExpendedora/Program.cs
+++ MaquinaExpendedora/Program.cs
@@ -12,22 +12,66 @@
  * - Debemos controlar que las monedas enviadas estén dentro de las soportadas.
  */
 
-int[] monedasSoportadas = { 5, 10, 50, 100, 200 };
-
-static void Menu()
-{
-    Console.WriteLine("Bienvenido a la máquina expendedora");
-    Console.WriteLine("Seleccione un producto:");
-    Console.WriteLine("1. Agua (100 céntimos)");
-    Console.WriteLine("2. Refresco (150 céntimos)");
-    Console.WriteLine("3. Snack (200 céntimos)");
-    Console.WriteLine("4. Salir");
-}
 
 
-static void MaquinaExpendedora(int cantidad, int[]monedas,int seleccion)
+using System.ComponentModel;
+using System.Diagnostics;
+
+class Program
 {
 
+   
+    
+    static int[] monedasSoportadas = { 5, 10, 50, 100, 200 };
+   
+    
+    static void Maquina(string name,int n,Dictionary<string,int> producto)
+    {
+        
+        foreach (var item in producto) {
+            if (name.Equals(item.Key)) {
+                if (n >= item.Value) {
+                   int total = n - item.Value;
+                   List<int>vuelto=devolucion(total);
+                   Console.WriteLine("su vuelto es " + total );
+                    foreach (var v in vuelto)
+                    {
+                        Console.WriteLine("sera devuelto en "+v);
+                    }
+
+                }
+            
+            }
+        }
+     
+    } 
+
+    static List<int> devolucion(int monto)
+    {
+        List<int> list = new List<int>();
+        foreach (var moneda in monedasSoportadas.OrderByDescending(x=>x))
+        {
+            while (monto >= moneda) {
+                list.Add(moneda);
+                monto -= moneda;
+            }
+        }
+        return list;
+    }
+
+    static void Main(string[] args)
+    {
+        Dictionary<string, int> productos = new Dictionary<string, int>()
+    {
+        {"Cockie",50 },
+        {"Agua",100 },
+        {"Snack",200 }
+
+    };
+        Maquina("Cockie", 500, productos);
+    }
+
 }
+
 
 
