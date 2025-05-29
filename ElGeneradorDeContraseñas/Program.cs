7e982c3796950415ea78f033156942d81eb19198
sanchezchandialf <lautarosanche@gmail.com>
2025-05-29 11:48:47 -0300

    two new challenge

---
+++ ElGeneradorDeContraseñas/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Escribe un programa que sea capaz de generar contraseñas de forma aleatoria.
+ * Podrás configurar generar contraseñas con los siguientes parámetros:
+ * - Longitud: Entre 8 y 16.
+ * - Con o sin letras mayúsculas.
+ * - Con o sin números.
+ * - Con o sin símbolos.
+ * (Pudiendo combinar todos estos parámetros entre ellos)
+ */
+
+
+
+public class Program
+{
+
+    public static void Main()
+    {
+        Random rand = new Random();
+        int numero = rand.Next(8, 16);
+        
+    }
+}
